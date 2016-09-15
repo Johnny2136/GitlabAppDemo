b0982eac20c87a2c79c73b9cdaa215c11c46cca0
Johnny Johnson <johnny2136@gmail.com>
2016-09-15 12:31:28 -0400

    Fixed Select to DB

--- Script/entryApp.js
+++ Script/entryApp.js
@@ -2,10 +2,31 @@
 
 var entryApp = angular.module('entryApp', []);
 
+
 function entryCtrl($scope, $http) {
+
+
 	var profile = {"firstName":"","lastName":""};
 
         $scope.profile = profile
+
+ $scope.$on('missionChangedEvent', function(event, args) {
+	profile.mission = args;
+	console.log('event received ' + event + ' - ' + args);
+});
+
+ $scope.$on('countryChangedEvent', function(event, args) {
+	profile.country = args;
+	console.log('event received ' + event + ' - ' + args);
+});
+
+ $scope.$on('postChangedEvent', function(event, args) {
+	profile.post = args;
+	console.log('event received ' + event + ' - ' + args);
+});
+
+
+
 
 	$scope.postData = function() {
 alert(JSON.stringify($scope.profile));
@@ -37,26 +58,38 @@ function StaticCtrl($scope) {
         if (selectedRegion != '') {
             console.log(selectedRegion);
             $scope.selectedRegion = selectedRegion;
-        }
+		 $scope.$emit('missionChangedEvent', selectedRegion);
+	   }
         else
         {
             $scope.selectedRegion = null;
             $scope.selectedCountry = null;
             $scope.suburb = null;
+		 $scope.$emit('missionChangedEvent', null);
         }
     }
     $scope.countryValueChanged = function () {
+
         var countryElement = document.getElementById("city");
         var selectedCountry = countryElement.options[countryElement.selectedIndex].value;
         if (selectedCountry != '') {
             console.log(selectedCountry);
             $scope.selectedCountry = selectedCountry;
+		 $scope.$emit('countryChangedEvent', selectedCountry);
+	
         }
         else {
             $scope.selectedCountry = null;
             $scope.suburb = null;
+		 $scope.$emit('countryChangedEvent', null);
+	
+	
         }
     }
+
+$scope.$watch('suburb', function (newValue, oldValue, scope) {
+    $scope.$emit('postChangedEvent', newValue);
+});
 
     $scope.countries = {
     "WHA": {
