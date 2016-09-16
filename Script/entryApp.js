64e23ebfb0b6dbb2f514251c3d23cfd01f1929a5
Johnny Johnson <johnny2136@gmail.com>
2016-09-15 21:15:01 -0400

    Update entryApp.js
    Ran code through a JSHint linter and fixed some code errors.

--- Script/entryApp.js
+++ Script/entryApp.js
@@ -1,140 +1,129 @@
-'use strict';
+var entryApp = angular.module("entryApp", []);
 
-var entryApp = angular.module('entryApp', []);
+entryApp.controller("entryCtrl", function($scope, $http) {
+    "use strict";
 
+    var profile = {"firstName":"","lastName":""};
 
-function entryCtrl($scope, $http) {
+    $scope.profile = profile;
 
+    $scope.$on("missionChangedEvent", function(event, args) {
+        profile.mission = args;
+        console.log("event received " + event + " - " + args);
+    });
 
-	var profile = {"firstName":"","lastName":""};
+    $scope.$on("countryChangedEvent", function(event, args) {
+        profile.country = args;
+        console.log("event received " + event + " - " + args);
+    });
 
-        $scope.profile = profile
-
- $scope.$on('missionChangedEvent', function(event, args) {
-	profile.mission = args;
-	console.log('event received ' + event + ' - ' + args);
-});
-
- $scope.$on('countryChangedEvent', function(event, args) {
-	profile.country = args;
-	console.log('event received ' + event + ' - ' + args);
-});
-
- $scope.$on('postChangedEvent', function(event, args) {
-	profile.post = args;
-	console.log('event received ' + event + ' - ' + args);
-});
-
-
-
-
-	$scope.postData = function() {
-alert(JSON.stringify($scope.profile));
-
-        $http({
-            method: 'POST',
-	    data: JSON.stringify($scope.profile),
-	    contentType: "application/json",
-            url: 'https://api.mongolab.com/api/1/databases/demoapp/collections/applications?apiKey=ED6t0jIvp7Q9dZLFTUXi6aMr8kUDjxFj'
-        }).success(function(data, status) {
-            alert("data was successfully added");
-
-        }).error(function(data, status, headers, config) {
-		alert("there was an error adding data");
+    $scope.$on("postChangedEvent", function(event, args) {
+        profile.post = args;
+        console.log("event received " + event + " - " + args);
     });
 
 
-	}
+    $scope.postData = function() {
 
-}
+        alert(JSON.stringify($scope.profile));
 
-function StaticCtrl($scope) {
+        $http({
+            method: "POST",
+                data: JSON.stringify($scope.profile),
+                contentType: "application/json",
+            url: "https://api.mongolab.com/api/1/databases/demoapp/collections/applications?apiKey=ED6t0jIvp7Q9dZLFTUXi6aMr8kUDjxFj"
+            }).success(function(data, status) {
 
-    $scope.regionValueChanged = function ()
-    {
+                alert("data was successfully added");
+
+            }).error(function(data, status, headers, config) {
+                alert("there was an error adding data");
+            });
+
+        };
+
+});
+
+entryApp.controller("StaticCtrl", function($scope) {
+
+    $scope.regionValueChanged = function (){
         var regionElement = document.getElementById("country");
-        
-        var selectedRegion = regionElement.options[regionElement.selectedIndex].value;
-        if (selectedRegion != '') {
+        var selectedRegion = regionElement.options[regionElement.selectedIndex].text;
+        if (selectedRegion !== '') {
             console.log(selectedRegion);
             $scope.selectedRegion = selectedRegion;
-		 $scope.$emit('missionChangedEvent', selectedRegion);
-	   }
-        else
-        {
+            $scope.$emit("missionChangedEvent", selectedRegion);
+        }
+        else{
             $scope.selectedRegion = null;
             $scope.selectedCountry = null;
             $scope.suburb = null;
-		 $scope.$emit('missionChangedEvent', null);
+            $scope.$emit("missionChangedEvent", null);
         }
-    }
+    };
     $scope.countryValueChanged = function () {
 
         var countryElement = document.getElementById("city");
-        var selectedCountry = countryElement.options[countryElement.selectedIndex].value;
-        if (selectedCountry != '') {
+        var selectedCountry = countryElement.options[countryElement.selectedIndex].text;
+        if (selectedCountry !== '') {
             console.log(selectedCountry);
             $scope.selectedCountry = selectedCountry;
-		 $scope.$emit('countryChangedEvent', selectedCountry);
-	
+            $scope.$emit("countryChangedEvent", selectedCountry);
         }
         else {
             $scope.selectedCountry = null;
             $scope.suburb = null;
-		 $scope.$emit('countryChangedEvent', null);
-	
-	
+            $scope.$emit("countryChangedEvent", null);
         }
-    }
+    };
 
-$scope.$watch('suburb', function (newValue, oldValue, scope) {
-    $scope.$emit('postChangedEvent', newValue);
+$scope.$watch("suburb", function (newValue, oldValue, scope) {
+    $scope.$emit("postChangedEvent", newValue);
 });
-
     $scope.countries = {
-    "WHA": {
-        "USA": [
-            "DC"
-        ],
-        "Brazil": [
-            "Brasilia",
-            "Recife",
-            "Rio de Janiero",
-            "Sao Paulo"
-        ],
-        "Mexico": [
-            "Ciudad Juarez",
-            "Guadalajara",
-            "Hermosillo",
-            "Matamoros",
-            "Merida",
-            "Mexico City",
-            "Monterrey",
-            "Nogales",
-            "Nuevo Laredo",
-            "Tijuana"
-        ]
-    },
-    "EAP": {
-        "China": [
-            "Beijing",
-            "Shanghai",
-            "Beijing",
-            "Chengdu",
-            "Guangzhou",
-            "Shanghai",
-            "Shenyang",
-            "Wuhan"
-        ]
-    },
-    "SCA": {
-        "India": [
-            "Chennai/Madras",
-            "Hyderabad",
-            "Kolkata",
-            "Mumbai/Bombay",
-            "New Delhi"
-        ]
-    }
-};
-}
+        "WHA": {
+            "USA": [
+                "DC"
+            ],
+            "Brazil": [
+                "Brasilia",
+                "Recife",
+                "Rio de Janiero",
+                "Sao Paulo"
+            ],
+            "Mexico": [
+                "Ciudad Juarez",
+                "Guadalajara",
+                "Hermosillo",
+                "Matamoros",
+                "Merida",
+                "Mexico City",
+                "Monterrey",
+                "Nogales",
+                "Nuevo Laredo",
+                "Tijuana"
+            ]
+        },
+        "EAP": {
+            "China": [
+                "Beijing",
+                "Shanghai",
+                "Beijing",
+                "Chengdu",
+                "Guangzhou",
+                "Shanghai",
+                "Shenyang",
+                "Wuhan"
+            ]
+        },
+        "SCA": {
+            "India": [
+                "Chennai/Madras",
+                "Hyderabad",
+                "Kolkata",
+                "Mumbai/Bombay",
+                "New Delhi"
+            ]
+        }
+    };
+});
